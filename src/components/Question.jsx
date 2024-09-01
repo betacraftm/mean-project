a8d956a5adacefeccb62328e72668f07021378d3
Betacraft <hoangkimc9@gmail.com>
2024-09-01 21:58:18 +0700

    build: comple ui for question and footer section

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,3 +1,45 @@
 export default function Question() {
-  return <div>Question</div>;
+  return (
+    <div id="questions" className="mx-auto max-w-7xl py-16">
+      <h2 className="mb-12 font-beautique text-5xl font-medium text-[#193F53]">
+        Một số câu hỏi thường gặp
+      </h2>
+      <div>
+        <ul className="flex flex-col gap-12 text-[#193F53]">
+          <li className="border-b border-black pb-5">
+            <h3 className="font-montserrat text-2xl leading-10">
+              1. Thời gian thiết kế 3D cho 1 công trình dự kiến là bao lâu ?
+            </h3>
+            <p className="indent-[22px] font-montserrat text-xl">
+              Mean có thể hoàn thiện thiết kế 3d từ 3-5 ngày tùy theo diện tích
+            </p>
+          </li>
+          <li className="border-b border-black pb-5">
+            <h3 className="font-montserrat text-2xl leading-10">
+              1. Thời gian thiết kế 3D cho 1 công trình dự kiến là bao lâu ?
+            </h3>
+            <p className="indent-[22px] font-montserrat text-xl">
+              Mean có thể hoàn thiện thiết kế 3d từ 3-5 ngày tùy theo diện tích
+            </p>
+          </li>
+          <li className="border-b border-black pb-5">
+            <h3 className="font-montserrat text-2xl leading-10">
+              1. Thời gian thiết kế 3D cho 1 công trình dự kiến là bao lâu ?
+            </h3>
+            <p className="indent-[22px] font-montserrat text-xl">
+              Mean có thể hoàn thiện thiết kế 3d từ 3-5 ngày tùy theo diện tích
+            </p>
+          </li>
+          <li className="border-b border-black pb-5">
+            <h3 className="font-montserrat text-2xl leading-10">
+              1. Thời gian thiết kế 3D cho 1 công trình dự kiến là bao lâu ?
+            </h3>
+            <p className="indent-[22px] font-montserrat text-xl">
+              Mean có thể hoàn thiện thiết kế 3d từ 3-5 ngày tùy theo diện tích
+            </p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
